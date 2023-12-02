dc90717c424423c08414e76507474708b3fc93d4
Matthew Morrison <james.morrison@asurion.com>
2023-12-02 11:40:52 -0600

    day 1 puzzle 1

---
+++ 1/puzzle1.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#define BUF_SIZE 512
+
+int getCalibrationValue(char *buf) {
+  char str[3];
+  str[2] = '\0';
+
+  // get first digit char
+  size_t i = 0;
+  while (buf[i] != '\0') {
+    if (isdigit(buf[i])) {
+      str[0] = buf[i];
+      break;
+    } 
+    ++i;
+  }
+
+  // get last digit char
+  while (buf[i] != '\0') {
+    if (isdigit(buf[i])) {
+      str[1] = buf[i];
+    }
+    ++i;
+  }
+  
+  // ascii to int
+  return atoi(str);
+}
+
+int main(void) {
+  FILE *fp;
+  char buf[BUF_SIZE];
+  int total = 0;
+
+  fp = fopen("input1.txt", "r");
+
+  if (fp == NULL) {
+    perror("Error opening file");
+    return 1;
+  }
+
+  while (fgets(buf, sizeof(buf), fp) != NULL) {
+    total += getCalibrationValue(buf);
+  }
+
+  printf("%d", total);
+
+  return 0;
+}
